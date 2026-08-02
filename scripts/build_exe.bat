1b1fcccdd47c8314cf12f2a422d1b3b3a790ddab
FuGuang <669744854@qq.com>
2026-08-02 14:06:30 +0800

    sync: scripts/build_exe.bat

--- scripts/build_exe.bat
+++ scripts/build_exe.bat
@@ -1,9 +1,6 @@
 @echo off
 cd /d "%~dp0.."
-echo 正在安装 PyInstaller...
-pip install pyinstaller -q
-echo 正在打包为单文件 exe...
-if exist MemWise.exe del MemWise.exe
-pyinstaller --onefile --noconsole --name MemWise --add-data "core;core" --distpath . memwise_gui.py
-echo 打包完成！exe 位于当前目录
+taskkill /f /im MemWise.exe >nul 2>&1
+pyinstaller MemWise.spec --distpath dist --workpath build --noconfirm
+echo 打包完成！exe 位于 dist 目录
 pause
